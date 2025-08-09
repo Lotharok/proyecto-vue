c15d20066e04a89e19d93aa52500f9e29e13efc7
Jesus <jesus.avila@pricetravel.com>
2025-08-08 23:48:40 -0500

    Initial Commit

---
+++ packages/utils/vite.config.ts
@@ -0,0 +1,37 @@
+/* eslint-disable no-undef */
+import path from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+   build: {
+      lib: {
+         entry: {
+            index: path.resolve(__dirname, "src/index.ts"),
+         },
+         formats: ["es"],
+      },
+      rollupOptions: {
+         output: {
+            preserveModules: true,
+            preserveModulesRoot: "src",
+            entryFileNames: "[name].js",
+         },
+      },
+      minify: process.env.NODE_ENV === "production",
+   },
+   plugins: [
+      dts({
+         include: ["src/**/*"],
+         exclude: ["**/*.test.ts", "**/*.spec.ts", "src/test-utils/**/*"],
+         insertTypesEntry: true,
+         copyDtsFiles: true,
+         rollupTypes: true,
+      }),
+   ],
+   resolve: {
+      alias: {
+         "@": path.resolve(__dirname, "./src"),
+      },
+   },
+});
---
+++ packages/vue-utils/vite.config.ts
@@ -0,0 +1,44 @@
+/* eslint-disable no-undef */
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+   build: {
+      lib: {
+         entry: path.resolve(__dirname, "src/index.ts"),
+         formats: ["es"],
+      },
+      rollupOptions: {
+         external: [
+            "vue",
+            "i18next",
+            "i18next-vue",
+            "i18next-http-backend",
+            "@pnpmworkspace/types",
+            "@pnpmworkspace/utils",
+         ],
+         output: {
+            preserveModules: true,
+            preserveModulesRoot: "src",
+            entryFileNames: "[name].js",
+            assetFileNames: (assetInfo) => {
+               if (assetInfo.name === "style.css") return "style.css";
+               return assetInfo.name || "asset-[hash][extname]";
+            },
+            globals: {
+               vue: "Vue",
+            },
+         },
+      },
+      cssCodeSplit: false,
+      minify: process.env.NODE_ENV === "production",
+   },
+   plugins: [vue(), dts({ exclude: ["**/*.test.ts", "**/*.spec.ts", "src/test-utils/**/*"] })],
+   resolve: {
+      alias: {
+         "@": path.resolve(__dirname, "./src"),
+      },
+   },
+});
